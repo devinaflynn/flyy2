a63312aed8758ecf685a8f8c38d73e98c7d41b5a
Devin Flynn <dfloveone@gmail.com>
2014-10-22 15:42:38 -0400

    user settings page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929031642) do
+ActiveRecord::Schema.define(version: 20141022165056) do
 
   create_table "apps", force: true do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140929031642) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
