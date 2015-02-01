34ea8a30d7aa6790f003feb9fe7dd181591fa0c5
Paulo Abreu <pauloalexabreu@gmail.com>
2015-02-01 13:51:22 +0000

    Add sign up confirmation through e-mail.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150201114326) do
+ActiveRecord::Schema.define(version: 20150201133756) do
 
   create_table "apps", force: true do |t|
     t.string   "name"
@@ -73,8 +73,13 @@ ActiveRecord::Schema.define(version: 20150201114326) do
     t.string   "social_github"
     t.string   "social_twitter"
     t.string   "social_facebook"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
